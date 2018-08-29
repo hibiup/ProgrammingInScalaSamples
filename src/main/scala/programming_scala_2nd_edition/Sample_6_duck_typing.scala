94c9b8492b4cc3d32dfba0b1d5d3af6d72fc4813
326487162 <Welcome@661>
2018-08-29 11:59:11 -0400

    Add sugar for private variable

--- src/main/scala/programming_scala_2nd_edition/Sample_6_duck_typing.scala
+++ src/main/scala/programming_scala_2nd_edition/Sample_6_duck_typing.scala
@@ -1,12 +1,10 @@
 package programming_scala_2nd_edition.sample_6
 
-import programming_scala_2nd_edition.sample_6.duck_typing.DuckShowColor
-
 object duck_typing extends App{
     // 鸭类
     trait Bird {
-        def fly:Unit = {None}
-        def twit:Unit = {None}
+        def fly:Unit = println("I can fly!")
+        def twit:Unit
     }
 
     trait Swimming {
@@ -19,15 +17,26 @@ object duck_typing extends App{
 
     trait Coloring {
         def color:String
+        /**
+          * 语法糖：这里是最容易引起“误解”的地方。
+          * 1) 首先，下划线（_）连接了 "color" 和 "=" 让他们连成同一个变量(函数)名："color_="
+          * 2) 其次，在引用这个变量（函数）的时候，下划线可以省略，也就是说。引用形式可以写成："color =" 或 "color="，等同于"color_="
+          *    需要注意的是，这种便利只是一个调用端的约定，并不意味着对象真的引出了 "color =" 或 "cclor=" 名称
+          *
+          * 参考： https://www.dustinmartin.net/getters-and-setters-in-scala:
+          */
+        def color_=(value:String) = println("No, you can't change it")   // "color_=" 是一个函数名，不是赋值操作
     }
 
     val duckFactory = (age:Int, c:String) => {    // 1）返回一个带有 trait Duck 接口的类实例
         if (age >= 18) new Bird with Swimming with Quacking with Coloring {
-            override def fly = println("I can fly!")
-            override def color:String = c
+            override def color = c
         }
         else new Bird with Swimming with Quacking with Coloring {
-            override def color:String = c
+            override def fly = println("I can't fly!")
+            private var _color = c
+            override def color:String = _color
+            override def color_=(value:String) = _color = value
         }
     }
 
@@ -35,6 +44,11 @@ object duck_typing extends App{
     def DuckTwit(duck: {def twit: Unit}) {duck.twit}
     def DuckSwimming(duck: {def swim: Unit}) {duck.swim}
     def DuckShowColor(duck: {def color: String}) {println(duck.color)}
+    /**
+      * 引入 "def color:String" 只是为了做类型匹配，实际上并没有被调用到。duck.color=c 实际上调用了 color_=() 方法。
+      * 利用的是 scala 方法或函数名称中下的划线在调用端可以省略的语法糖。
+      * */
+    def ChangeColor(duck: {def color_=(value:String); def color:String})(c:String) { duck.color=c }
 
     val childDuck = duckFactory(1, "YELLO")
     val adultDuck = duckFactory(18, "BLACK")
@@ -43,11 +57,14 @@ object duck_typing extends App{
     DuckTwit(childDuck)
     DuckFly(childDuck)
     DuckShowColor(childDuck)
+    ChangeColor(childDuck)("RED")
+    DuckShowColor(childDuck)
 
     DuckSwimming(adultDuck)
     DuckTwit(adultDuck)
     DuckFly(adultDuck)
     DuckShowColor(adultDuck)
+    ChangeColor(adultDuck)("GREEN")
 }
 
 object enhanced_with_implicate extends App{
