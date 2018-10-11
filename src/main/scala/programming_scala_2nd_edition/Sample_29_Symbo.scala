6ef0ca5eb0a318545649541d75d19009cb8213f0
326487162 <Welcome@661>
2018-10-11 11:34:47 -0400

    Add Symbol samples

---
+++ src/main/scala/programming_scala_2nd_edition/Sample_29_Symbo.scala
@@ -0,0 +1,26 @@
+package programming_scala_2nd_edition
+
+package Sample_29_Symbo {
+    object SymboTest {
+        /** Symbol: 符号字面量类型。语法为 '<标识符>
+          *
+          * 符号字面量典型的应用场景是在动态语言中作为一个标识符。比如要执行数据库操作，参数可能是一个字段名，比如 firstName，
+          * 动态语言可能直接使用 firstName 作为参数：
+          *
+          *   updateRecord(firstName, "xxx")
+          *
+          * 这在静态类型语言中会出错，因为 firstName　未
+          * 被定义过。作为提到的方法，可以将参数定义成 Symbol 类型。然后调用的时候使用：
+          *
+          *   pdateRecord('firstName, "xxx")
+          *
+          * */
+        def updateRecord(col_name: Symbol, value: Any): Unit = {
+            println(col_name, col_name.name)
+        }
+
+        def apply() {
+            updateRecord('firstName, "Jeff")
+        }
+    }
+}
---
+++ src/test/scala/programming_scala_2nd_edition/Sample_29_Symbo_test.scala
@@ -0,0 +1,10 @@
+package programming_scala_2nd_edition
+
+import org.scalatest.FunSuite
+import programming_scala_2nd_edition.Sample_29_Symbo.SymboTest
+
+class Sample_29_Symbo_test extends FunSuite{
+    test("Symbo test") {
+        SymboTest()
+    }
+}
