c984006785e6f037b306c8d0def8c55c0437c8f2
326487162 <Welcome@661>
2018-12-03 12:14:35 -0500

    Add example for Future to avoid Stackoverflow

--- src/test/scala/programming_scala_2nd_edition/Sample_26_Future_test.scala
+++ src/test/scala/programming_scala_2nd_edition/Sample_26_Future_test.scala
@@ -3,6 +3,9 @@ package programming_scala_2nd_edition.Sample_26_Future
 import org.scalatest.FunSuite
 import programming_scala_2nd_edition.DynamicFutureReturnType.Compute
 
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.forkjoin.ForkJoinPool
+
 class Sample_26_Future_test extends FunSuite{
     test("Future callback") {
         FutureCallbackFunctions()
@@ -34,5 +37,19 @@ class Sample_26_Future_test extends FunSuite{
 
     test("Test Future Future exception") {
         FutureException()
+    }
+
+    test("Future should be able to avoid Stackoverflow") {
+        implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(new ForkJoinPool(1))
+        case class Round(count: Int)
+
+        def playGame[T]: Round => Future[T] = (hit) => Future { playGame {
+            (new RuntimeException(hit.count.toString)).printStackTrace()
+            Round( hit.count + 1)
+        }}(ec).asInstanceOf[Future[T]]
+
+        playGame(Round(1))
+
+        Thread.sleep(100)
     }
 }
